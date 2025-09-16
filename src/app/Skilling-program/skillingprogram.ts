36480e71931e07bad082646244df564e2f1ab5cc
aravindh shankar <aravindhshankar@tansam.org>
2025-09-16 11:15:49 +0530

    Changes in aboutuspage

---
+++ src/app/Skilling-program/skillingprogram.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-skillingprogram',
+  imports:[],
+  standalone:true,
+  templateUrl:'./skillingprogram.html',
+  styleUrl:'./skillingprogram.css'
+})
+
+export class SkillingProgram{
+
+}
