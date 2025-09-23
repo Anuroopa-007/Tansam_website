b1e45128db2c575da8768892c5b225247a30adea
aravindh shankar <aravindhshankar@tansam.org>
2025-09-23 12:09:40 +0530

    Added news and evenets

--- src/app/homepage/newsletter/newsletter.component.ts
+++ src/app/homepage/newsletter/newsletter.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
@@ -5,8 +6,31 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-newsletter',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './newsletter.component.html',
   styleUrl: './newsletter.component.css',
 })
-export class NewsletterComponent {}
+export class NewsletterComponent {
+  newsletterItems = [
+    {
+      title: 'img',
+      desc: 'img',
+      image: 'assets/bannerimage/entrepre-1.jpeg'
+    },
+    {
+      title: 'img',
+      desc: 'img',
+      image: 'assets/bannerimage/entrepre-1.jpeg'
+    },
+    {
+      title: 'img',
+      desc: 'img',
+      image: 'assets/bannerimage/entrepre-1.jpeg'
+    },
+    {
+      title: 'img',
+      desc: 'img',
+      image: 'assets/bannerimage/entrepre-1.jpeg'
+    },
+  ];
+}
