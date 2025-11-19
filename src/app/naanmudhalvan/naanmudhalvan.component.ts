0a66e9585d9bbab6ca1cae275a70ad141b79bbf3
Arvindh Shankar <161598154+AravindhshankarTansam@users.noreply.github.com>
2025-11-19 22:35:45 +0530

    hackathon image and content added

--- src/app/naanmudhalvan/naanmudhalvan.component.ts
+++ src/app/naanmudhalvan/naanmudhalvan.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
@@ -23,12 +23,19 @@ export class NaanmudhalvanComponent implements OnInit {
   currentTestimonialIndex = 0;
   trainingCurrentIndex = 0;
 
+  naanmudhalvan_events: any[] = [];
+
+
+  @ViewChildren('videoPlayer') testimonialVideos!: QueryList<ElementRef<HTMLVideoElement>>;
+  @ViewChildren('trainingVideoPlayer') trainingVideosRefs!: QueryList<ElementRef<HTMLVideoElement>>;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.loadTableData();
     this.loadTestimonials();
     this.loadTrainingVideos();
+    this.loadEvents();
   }
 
   loadTableData() {
@@ -86,6 +93,13 @@ export class NaanmudhalvanComponent implements OnInit {
       });
   }
 
+  // Load Events JSON
+loadEvents() {
+  this.http.get<any[]>('/assets/Json/naanmudhalvan_events.json').subscribe(response => {
+    this.naanmudhalvan_events = response;
+  });
+}
+
     // Carousel helper
   get visibleTestimonials() {
     const total = this.testimonials.length;
@@ -98,14 +112,29 @@ export class NaanmudhalvanComponent implements OnInit {
     ];
   }
 
-  prevTestimonial() {
-    this.currentTestimonialIndex =
-      (this.currentTestimonialIndex - 1 + this.testimonials.length) % this.testimonials.length;
+    pauseOtherVideos(type: 'testimonial' | 'training', centerIndex: number) {
+    if (type === 'testimonial') {
+      this.testimonialVideos.forEach((video, index) => {
+        if (index !== centerIndex) video.nativeElement.pause();
+      });
+    } else {
+      this.trainingVideosRefs.forEach((video, index) => {
+        if (index !== centerIndex) video.nativeElement.pause();
+      });
+    }
   }
+
 
   nextTestimonial() {
     this.currentTestimonialIndex =
       (this.currentTestimonialIndex + 1) % this.testimonials.length;
+    setTimeout(() => this.autoplayCenterVideo('testimonial'), 50);
+  }
+
+  prevTestimonial() {
+    this.currentTestimonialIndex =
+      (this.currentTestimonialIndex - 1 + this.testimonials.length) % this.testimonials.length;
+    setTimeout(() => this.autoplayCenterVideo('testimonial'), 50);
   }
 
   get visibleTrainingVideos() {
@@ -119,13 +148,31 @@ export class NaanmudhalvanComponent implements OnInit {
   ];
 }
 
-prevTraining() {
-  this.trainingCurrentIndex =
-    (this.trainingCurrentIndex - 1 + this.trainingVideos.length) % this.trainingVideos.length;
-}
+  nextTraining() {
+    this.trainingCurrentIndex =
+      (this.trainingCurrentIndex + 1) % this.trainingVideos.length;
+    setTimeout(() => this.autoplayCenterVideo('training'), 50);
+  }
 
-nextTraining() {
-  this.trainingCurrentIndex =
-    (this.trainingCurrentIndex + 1) % this.trainingVideos.length;
-}
+  prevTraining() {
+    this.trainingCurrentIndex =
+      (this.trainingCurrentIndex - 1 + this.trainingVideos.length) % this.trainingVideos.length;
+    setTimeout(() => this.autoplayCenterVideo('training'), 50);
+  }
+
+  autoplayCenterVideo(type: 'testimonial' | 'training') {
+    if (type === 'testimonial') {
+      this.testimonialVideos.forEach((video, index) => {
+        if (index === 1) video.nativeElement.play();
+        else video.nativeElement.pause();
+      });
+    } else {
+      this.trainingVideosRefs.forEach((video, index) => {
+        if (index === 1) video.nativeElement.play();
+        else video.nativeElement.pause();
+      });
+    }
+  }
+
+
 }
